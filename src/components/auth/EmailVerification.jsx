0682e8b5f5f47c00958e78932e51a0b6e7400064
Adamya Singh <adamya45@gmail.com>
2023-07-19 12:57:57 +0530

    moving to next otp field

--- src/components/auth/EmailVerification.jsx
+++ src/components/auth/EmailVerification.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Container from "../Container";
 import Title from "../form/Title";
 import Submit from "../form/Submit";
@@ -7,6 +7,22 @@ const OTP_LENGTH = 6;
 
 export default function EmailVerification() {
   const [otp, setOtp] = useState(new Array(OTP_LENGTH).fill(""));
+  const [activeOtpIndex, setActiveOtpIndex] = useState(0);
+
+  const inputRef = useRef();
+
+  const handleOtpChange = ({target}, index) => {
+    const {value} = target;
+    //setOtp([value]);
+    setActiveOtpIndex(index + 1);
+  };
+
+  useEffect(() => {
+
+    inputRef.current?.focus();
+  }, [activeOtpIndex]);
+
+  
   return (
     <div className="fixed inset-0 bg-primary -z-10  flex justify-center items-center">
       <Container>
@@ -17,11 +33,16 @@ export default function EmailVerification() {
               OTP has been sent to your registered email
             </p>
           </div>
+
           <div className="flex justify-center items-center space-x-4">
             {otp.map((_, index) => {
               return (
                 <input
+                ref={activeOtpIndex === index ? inputRef:null}
+                  key={index}
                   type="number"
+                  value={otp[index] || ""}
+                  onChange={(ev) => handleOtpChange(ev, index)}
                   className="w-12 h-12 border-2 border-dark-subtle focus:border-white rounded bg-transparent outline-none text-center text-white font-semibold text-xl spin-button-none"
                 />
               );
