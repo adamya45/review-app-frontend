df05915a5ce0b043748ef2f5657d5408473189e3
Adamya Singh <adamya45@gmail.com>
2023-07-20 12:44:09 +0530

    linked both frontend and backend using CORS package and completed signup process by linking frontend and backend

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,24 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import { createUser } from "../../api/auth";
+import { commonModalClasses } from "../../utils/theme";
 import Container from "../Container";
-import Title from "../form/Title";
+import CustomLink from "../CustomLink";
+import FormContainer from "../form/FormContainer";
 import FormInput from "../form/FormInput";
 import Submit from "../form/Submit";
-import CustomLink from "../CustomLink";
-import { commonModalClasses } from "../../utils/theme";
-import FormContainer from "../form/FormContainer";
+import Title from "../form/Title";
+
+const validateUserInfo = ({ name, email, password }) => {
+  const isValidEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const isValidName = /^[a-z A-Z]+$/;
+
+  if (!name.trim()) return { ok: false, error: "Name is missing!" };
+  if (!isValidName.test(name)) return { ok: false, error: "Invalid name!" };
+
+  if (!email.trim()) return { ok: false, error: "Email is missing!" };
+  if (!isValidEmail.test(email)) return { ok: false, error: "Invalid email!" };
+
+  if (!password.trim()) return { ok: false, error: "Password is missing!" };
+  if (password.length < 8)
+    return { ok: false, error: "Password must be 8 characters long!" };
+
+  return { ok: true };
+};
 
 export default function Signup() {
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { ok, error } = validateUserInfo(userInfo);
+
+    if (!ok) return console.log(error);
+
+    const response = await createUser(userInfo);
+    if (response.error) return console.log(response.error);
+    console.log(response.user);
+  };
+
+  const { name, email, password } = userInfo;
+
   return (
     <FormContainer>
       <Container>
-        <form className={commonModalClasses + " w-72"}>
+        <form onSubmit={handleSubmit} className={commonModalClasses + " w-72"}>
           <Title>Sign up</Title>
-          <FormInput label="Name" placeholder="Abc" name="name" />
-          <FormInput label="Email" placeholder="abc@gmail.com" name="email" />
-          <FormInput label="Password" placeholder="●●●●●●●●" name="password" />
+          <FormInput
+            value={name}
+            onChange={handleChange}
+            label="Name"
+            placeholder="John Doe"
+            name="name"
+          />
+          <FormInput
+            value={email}
+            onChange={handleChange}
+            label="Email"
+            placeholder="john@email.com"
+            name="email"
+          />
+          <FormInput
+            value={password}
+            onChange={handleChange}
+            label="Password"
+            placeholder="********"
+            name="password"
+            type="password"
+          />
           <Submit value="Sign up" />
+
           <div className="flex justify-between">
-            <CustomLink to="/auth/forget-password">Forget Password</CustomLink>
+            <CustomLink to="/auth/forget-password">Forget password</CustomLink>
             <CustomLink to="/auth/signin">Sign in</CustomLink>
           </div>
         </form>
