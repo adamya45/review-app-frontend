fb4074b0d0b44af9e49cd85efe9620ce857a9c5b
Adamya Singh <adamya45@gmail.com>
2023-08-01 13:52:32 +0530

    handled invalid and valid token while reseting password

--- src/components/auth/ConfirmPassword.jsx
+++ src/components/auth/ConfirmPassword.jsx
@@ -1,19 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Container from "../Container";
 import Title from "../form/Title";
 import FormInput from "../form/FormInput";
 import Submit from "../form/Submit";
 import FormContainer from "../form/FormContainer";
 import { commonModalClasses } from "../../utils/theme";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { PiSpinnerGapBold } from "react-icons/pi";
+import { verifyPasswordResetToken } from "../../api/auth";
+import { useNotification } from "../../hooks";
 
 export default function ConfirmPassword() {
+  const [isVerifying, setIsVerifying] = useState(true);
+  const [isValid, setIsValid] = useState(false);
   const [searchParams] = useSearchParams();
   const token = searchParams.get("token");
   const id = searchParams.get("id");
-  console.log(token, id);
 
-  //http://localhost:3000/auth/reset-password?token=f31ee4fb391dd84e8d6da0eca070ec8bf26aebdc2684cdcef88c0edeea08&id=64c7fd642781947dc0c63e01
+  const { updateNotification } = useNotification();
+  const navigate = useNavigate();
+
+  // isValid, !isValid
+
+  useEffect(() => {
+    isValidToken();
+  }, []);
+
+  const isValidToken = async () => {
+    const { error, valid } = await verifyPasswordResetToken(token, id);
+    setIsVerifying(false);
+    if (error) {
+      return updateNotification("error", error);
+    }
+    if (!valid) {
+      setIsValid(false);
+      setIsVerifying(false);
+      return navigate("/auth/reset-password", { replace: true });
+    }
+    setIsValid(true);
+  };
+
+  if (isVerifying)
+    return (
+      <FormContainer>
+        <Container>
+          <div className="flex space-x-2 items-center">
+            <h1 className="text-4xl font-semibold dark:text-white text-primary">
+              Please wait, while we are verifying your token!
+            </h1>
+            <PiSpinnerGapBold className="animate-spin text-4xl dark:text-white text-primary" />
+          </div>
+        </Container>
+      </FormContainer>
+    );
+
+  if (!isValid)
+    return (
+      <FormContainer>
+        <Container>
+          <h1 className="text-4xl font-semibold dark:text-white text-primary">
+            Sorry, the token is invalid!
+          </h1>
+        </Container>
+      </FormContainer>
+    );
 
   return (
     <FormContainer>
