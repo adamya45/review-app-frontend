9dc6c2285506002fa0ec45c97c5debce39cee9fd
Adamya Singh <adamya45@gmail.com>
2023-08-02 17:56:34 +0530

    conditional rendering resenr email

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useAuth } from "../hooks";
+import Container from "./Container";
 
 export default function Home() {
+  const { authInfo } = useAuth();
+  const { isLoggedIn } = authInfo;
+  const isVerified = authInfo.profile?.isVerified;
+
+  console.log(authInfo);
   return (
-    <div>Home</div>
-  )
+    <Container>
+      {isLoggedIn && !isVerified ? (
+        <p className="text-lg text-center bg-blue-50 p-2">
+          Your account is not verified,{" "}
+          <button className="text-blue-500 font-semibold hover:underline">
+            Click here to verify your account.
+          </button>
+        </p>
+      ) : null}
+    </Container>
+  );
 }
